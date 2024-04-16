8d4c521801828f50a1942314acd1b0e07406f87e
F33RNI <xxoinvizionxx@gmail.com>
2024-04-15 19:50:38 -0500

    style: add project link to the files header

--- include/petal.h
+++ include/petal.h
@@ -5,6 +5,8 @@
  *
  * @copyright Copyright (c) 2023-2024 Fern Lane
  *
+ * This file is part of the PetalFlow distribution <https://github.com/F33RNI/PetalFlow>.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
